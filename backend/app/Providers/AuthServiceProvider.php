ca842b242de5a043c3a4fd4392fb7dec81e24cd6
James Andrei Revilla <revillajamesandrei4@gmail.com>
2026-07-03 21:20:59 +0800

    feat(auth): register application policies

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,24 @@
 
 namespace App\Providers;
 
+use App\Models\AttendanceLog;
+use App\Models\DailyReport;
+use App\Models\DocumentationFile;
+use App\Models\Evaluation;
+use App\Models\Organization;
+use App\Models\Program;
+use App\Models\User;
+use App\Models\UserProgram;
+use App\Models\WeeklyReport;
+use App\Policies\AttendanceLogPolicy;
+use App\Policies\DailyReportPolicy;
+use App\Policies\DocumentationFilePolicy;
+use App\Policies\EvaluationPolicy;
+use App\Policies\OrganizationPolicy;
+use App\Policies\ProgramPolicy;
+use App\Policies\UserPolicy;
+use App\Policies\UserProgramPolicy;
+use App\Policies\WeeklyReportPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +31,15 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        UserProgram::class => UserProgramPolicy::class,
+        AttendanceLog::class => AttendanceLogPolicy::class,
+        DailyReport::class => DailyReportPolicy::class,
+        WeeklyReport::class => WeeklyReportPolicy::class,
+        DocumentationFile::class => DocumentationFilePolicy::class,
+        Evaluation::class => EvaluationPolicy::class,
+        User::class => UserPolicy::class,
+        Organization::class => OrganizationPolicy::class,
+        Program::class => ProgramPolicy::class,
     ];
 
     /**
